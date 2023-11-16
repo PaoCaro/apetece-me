91535f97f354922e23135ffcecf9fae8e0fa6cd9
tiaguitos0 <42713181+PaoCaro@users.noreply.github.com>
2023-11-16 13:59:36 +0000

    Initial commit

---
+++ C/h.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+  char frase[30];
+
+  printf("vá escreve ai uma merda qualquer:\n");
+  fgets(frase, sizeof(frase), stdin);
+
+  printf("%s", frase);
+
+  return 0;
+}
---
+++ Java/Main.java
@@ -0,0 +1,6 @@
+package Java;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
